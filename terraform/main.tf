e542fcf5182065f068c8419cd7555335170d8cd0
Funmi123 <ajanakueniola@gmail.com>
2026-02-20 13:44:58 +0100

    corrected repo2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "tf-lock"
+  billing_mode = "FREE_TIER"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 # ----------------------------
 # IAM ROLE FOR SSM
 # ----------------------------
