136ff9632f8fdf88d07801bf1360232978731381
Funmi123 <ajanakueniola@gmail.com>
2026-02-21 12:04:32 +0100

    corrections made on vpc.tf,main.tf and output.tf 13

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,6 +76,18 @@ resource "aws_lb_target_group_attachment" "app_attach" {
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = { for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => dvo }
+  zone_id  = data.aws_route53_zone.main.zone_id
+  name     = each.value.resource_record_name
+  type     = each.value.resource_record_type
+  records  = [each.value.resource_record_value]
+  ttl      = 60
 }
 
 # ALB Listener HTTPS
@@ -83,7 +95,9 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.cert.arn
+
+  certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
