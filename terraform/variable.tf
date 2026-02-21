84e1c34b9ae6b452977ed044ed308ed318f99e2d
Funmi123 <ajanakueniola@gmail.com>
2026-02-21 10:51:50 +0100

    corrections made on vpc.tf,main.tf and output.tf 8

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -4,11 +4,6 @@ variable "key_name" {}
 variable "ami_id" {}
 variable "db_password" {
   type = string
-
-  validation {
-    condition     = length(var.db_password) >= 8
-    error_message = "Password must be at least 8 characters."
-  }
 }
 variable "domain_name" {
   description = "Your domain for ACM certificate (e.g., ennyontop1.duckdns.org)"
