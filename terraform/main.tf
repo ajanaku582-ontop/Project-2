465129a344e1c52a0373e63bdde6567a79da4990
Funmi123 <ajanakueniola@gmail.com>
2026-02-21 07:12:39 +0100

    new resource added to create AWS ARN

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,6 +91,19 @@ resource "aws_db_instance" "postgres" {
   skip_final_snapshot = true
 }
 
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "ennyontop1.duckdns.org"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "my-cert"
+  }
+}
+
 # ----------------------------
 # APPLICATION LOAD BALANCER
 # ----------------------------
@@ -129,7 +142,7 @@ resource "aws_lb_listener" "https" {
   protocol          = "HTTPS"
 
   ssl_policy      = "ELBSecurityPolicy-2016-08"
-  certificate_arn = var.acm_cert_arn
+  certificate_arn = aws_acm_certificate.cert.arn
 
   default_action {
     type             = "forward"
