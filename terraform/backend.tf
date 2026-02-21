385663723311e9a241208b88f39b1adf37d135e3
Funmi123 <ajanakueniola@gmail.com>
2026-02-21 09:40:42 +0100

    corrections made on vpc.tf,main.tf and output.tf 2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "eniola-cicd-state-bucket"
-    key            = "secure-app/terraform.tfstate"
+    key            = "secure-app-1/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "tf-lock"
     profile        = "east2"
