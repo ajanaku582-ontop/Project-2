e5858e9924f250f85d7a143aefec54a187962b7a
Funmi123 <ajanakueniola@gmail.com>
2026-02-21 11:41:09 +0100

    corrections made on vpc.tf,main.tf and output.tf 12

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,7 +1,6 @@
 variable "region" { default = "us-east-2" }
 variable "instance_type" { default = "c7i-flex.large" }
 variable "key_name" {}
-variable "ami_id" {}
 variable "db_password" {
   type = string
 }
