32264668ad7b3b396d020ff6f3ff4a2207cf78b3
Funmi123 <ajanakueniola@gmail.com>
2026-02-24 18:21:19 +0100

    corrections made on vpc.tf to accomodate bastion sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,16 @@ resource "aws_instance" "app" {
   tags = { Name = "app-server" }
 }
 
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public1.id
+  key_name      = var.key_name
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  tags = { Name = "Bastion-server" }
+}
+
 # ALB
 resource "aws_lb" "alb" {
   name               = "app-alb"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -89,6 +89,40 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# Bastion SG
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ALB SG
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port       = 8000
+    to_port         = 8000  
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # DB SG
 resource "aws_security_group" "db_sg" {
   vpc_id = aws_vpc.main.id
