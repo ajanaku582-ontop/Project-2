1607cabb2587c7bb182bb576eb51b883166ac16a
Funmi123 <ajanakueniola@gmail.com>
2026-02-24 18:36:56 +0100

    corrections made on main.tf to use user data 2

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -106,23 +106,6 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-# ALB SG
-resource "aws_security_group" "alb_sg" {
-  vpc_id = aws_vpc.main.id
-  ingress {
-    from_port       = 8000
-    to_port         = 8000  
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # DB SG
 resource "aws_security_group" "db_sg" {
   vpc_id = aws_vpc.main.id
